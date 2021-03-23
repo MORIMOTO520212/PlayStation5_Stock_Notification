a67fd406bbe84e1b4fa21f38bea0b8dd16623035
MORIMOTO520212 <kanten520212@gmail.com>
2021-03-23 16:46:11 +0900

    2021.03.23

---
+++ README.md
@@ -0,0 +1,13 @@
+# PlayStation5入荷通知プログラム
+
+対象サイトリンク：https://books.rakuten.co.jp/rb/16462859/?bkts=1  
+
+メーカー品番：CFI-1000A01  
+通知：Discord  
+API：楽天ブックスゲーム検索API  
+applicationId ：1031104505438937230  
+
+## 動作方法
+1. コマンドプロンプトを開いて`pip install discord`と打ってください.
+2. main.pyを実行させてください.
+プログラムを終了させるにはCtrl+Cと打ってください.
---
+++ main.py
@@ -0,0 +1,98 @@
+import requests, json
+import discord
+from time import sleep
+
+print("\n\nPlayStation5 Stock Notification")
+
+with open("db.json", "r") as f:
+    db = json.load(f)
+
+if not db["channelId"]:
+    print("初期設定をします.")
+    print("下のリンクへアクセスし、Discord Botを任意のサーバーへ参加させてください.")
+    input(db["inviteUrl"]+"\n参加させたらエンターキーを押してください.")
+    print("\n発言するチャンネルの設定を行います.")
+    print("--- チャンネルIDを探す ---")
+    print("1. ユーザ設定 > テーマ > 詳細設定の開発者モードをオン")
+    print("2. 任意のチャンネルを右クリック > IDをコピー")
+    print("3. チャンネルIDを下に貼り付ける")
+    print("------------------------")
+    channelId = int(input("チャンネルIDを貼り付けてください >"))
+    db["channelId"] = channelId
+    with open("db.json", "w") as f:
+        json.dump(db ,f, indent=4)
+    print("初期設定が完了しました.\n")
+
+# Discord Channel ID here. (int)
+channelId = db["channelId"]
+# Discord Bot Token
+token = db["token"]
+# API params
+params = {
+    "format": "json",
+    "makerCode": db["makerCode"],
+    "applicationId": db["applicationId"]
+}
+# True-在庫あり False-入荷待ち
+productStatus = 2
+
+client = discord.Client()
+
+async def rakuten_api(): # get responce
+    res = requests.get("https://app.rakuten.co.jp/services/api/BooksGame/Search/20170404", params=params)
+    res_json = json.loads(res.text)
+    return res_json
+
+async def send(msg): # message send
+    channel = client.get_channel(channelId)
+    await channel.send(msg)
+
+async def notification(res_json):
+    if res_json:
+        title     = str(res_json["Items"][0]["Item"]["title"])          # product name
+        hardware  = str(res_json["Items"][0]["Item"]["hardware"])       # hardware
+        itemPrice = "￥"+str(res_json["Items"][0]["Item"]["itemPrice"]) # product price
+        msg = f"在庫あり\n商品：{title}\nハードウェア：{hardware}\n価格：{itemPrice}"
+        await send(msg)
+    else:
+        await send("入荷待ち")
+
+async def detector():
+    global productStatus
+    while True:
+        try:
+            res_json = await rakuten_api()
+            if res_json["hits"]:
+                if not productStatus or 2==productStatus:
+                    await notification(res_json)
+                    productStatus = True
+            elif productStatus:
+                await notification(0) # 入荷待ち
+                productStatus = False
+            sleep(1)
+        except Exception as e:
+            if("Failed to establish a new connection" in str(e)): continue
+            print(str(e))
+            open("system_log.txt", "a").write(str(e)+"\n")
+            break
+        except KeyboardInterrupt:
+            print("exit.")
+            break
+
+@client.event
+async def on_message(msg):
+    if "/test" == msg.content:
+        await msg.channel.send("ここで発言します.")
+
+@client.event
+async def on_ready():
+    print("ユーザー名:",client.user.name)
+    print("ユーザーID：",client.user.id)
+    print("メーカー品番：",db['makerCode'])
+    print("アプリケーションID：",db['applicationId'])
+    print("稼働中...\n")
+    print("終了するには Ctrl+C")
+    await detector()
+
+if '__main__' == __name__:
+    client.run(token)
